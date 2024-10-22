aee9f340b18dbed3afdb617f30aed348e8d55d81
John Polychronopoulos <46429733+JohnnyPol@users.noreply.github.com>
2024-10-22 16:55:24 +0300

    Update challenge_8_script.sh

--- challenge_8_script.sh
+++ challenge_8_script.sh
@@ -9,7 +9,7 @@ python -c "with open('bf00', 'r+b') as f: f.seek(1073741824); f.write(b'Hello, t
 # Loop to create symbolic links from bf01 to bf09 pointing to bf00
 for i in $(seq -w 1 9); do
     ln bf00 bf0$i
-    echo "Created symbolic link: bf0$i -> bf00"
+    echo "Created hard link: bf0$i -> bf00"
 done
 
 ./riddle
